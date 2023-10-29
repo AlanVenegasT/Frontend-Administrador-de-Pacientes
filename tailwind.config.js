fdddebb6a24cec112cad21ee52f085a89bbe49d2
AlanVenegasT <alan4444v@gmail.com>
2023-10-29 02:51:28 -0600

    Modificacion tailwind.config,js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html", "./src/**/*.jsx"],  //Desarrollo:["./index.html", "./src/**/*.jsx"], 
+  content: ["./index.html", "./src/**/*.jsx"],  //Desarrollo:["./index.html", "./src/**/*.jsx"],    //Para generar el dist de produccion:["index.html", "./src/**/*.jsx"],
   theme: {
     extend: {},
   },
