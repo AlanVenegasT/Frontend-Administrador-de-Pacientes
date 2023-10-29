373b1c19e2c981cd4929c260d25818069552849a
AlanVenegasT <alan4444v@gmail.com>
2023-10-29 02:49:13 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./src/**/*.jsx"],  //Desarrollo:["./index.html", "./src/**/*.jsx"], 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
